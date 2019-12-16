f7473009634eb5f6a9bf58a8faa56ee3360a931a
DanieleVBella <danielebella2000@gmail.com>
2019-12-16 01:11:40 +0000

    Added headers file
    Includes very basic information about the project

---
+++ ADS131M04.h
@@ -0,0 +1,17 @@
+/* This is a library for the ADS131M04 4-channel ADC
+   
+   Product information:
+   https://www.ti.com/product/ADS131M04
+
+   Datasheet: 
+   https://www.ti.com/lit/gpn/ads131m04
+
+   This library was made for Imperial College London Rocketry
+   Created by Daniele Valentino Bella
+
+   */
+
+#ifndef ADS131M04_H
+#define ADS131M04_H
+
+#endif
